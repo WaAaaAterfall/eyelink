e0a97b459d6a0dd6935dd2738ad3b25ff1f79272
WaAaaAterfall <eleanor1025pan@outlook.com>
2023-06-16 13:12:09 -0400

    push

--- methods for now/raw_data/concatenate.py
+++ methods for now/raw_data/concatenate.py
@@ -6,7 +6,7 @@ def run(day):
     df_csv_append = pd.DataFrame()
     #day = '0307'
     sections = ['01', '02', '03', '04', '05']
-    csv_files = ["" + day + section + '/final_data.csv' for section in sections]
+    csv_files = ["" + day + "/" + day + section + '/final_data.csv' for section in sections]
     # append the CSV files
     df_csv_concat = pd.concat([pd.read_csv(file) for file in csv_files ], ignore_index=True)
     df_csv_concat.iloc[:, 1:].to_csv("" + day + "/final_data.csv")
@@ -16,8 +16,9 @@ def merge_beh(monkey, day):
     path = day
     os.chdir(path)
     csv_files = glob.glob('*{}.{}'.format('result','csv'))
+    print(csv_files)
     df_csv_concat = pd.concat([pd.read_csv(file) for file in csv_files ], ignore_index=True)
     df_csv_concat.iloc[:, 1:].to_csv("" + monkey + day + "_behavior.csv")
 
 if __name__ == '__main__':
-    merge_beh('J', '0307')
+    run('0225')
