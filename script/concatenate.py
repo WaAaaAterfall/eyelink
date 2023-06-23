388ba148db73948f807c51ddd1a6f66d0852993c
WaAaaAterfall <eleanor1025pan@outlook.com>
2023-06-23 18:11:00 -0400

    syn

--- script/concatenate.py
+++ script/concatenate.py
@@ -16,17 +16,12 @@ def merge_one_day_eye(day, monkey):
     df_csv_concat.iloc[:, 1:].to_csv(expect_dir + "/final_data.csv")
 
 
-def merge_eye(days):
-    record = []
-    df_csv_append = pd.DataFrame()
-    for day in days:
-        print(day)
-        record = merge_one_day_eye(day, record)
-    csv_files = ["" + day + '/final_data.csv' for day in days]
+def merge_eye(days, monkey):
+    expect_dir = f'C:/Users/River/23summer/dku/edf_monkey/Data/{monkey}/'
+    csv_files = ['' + expect_dir + day + '/final_data.csv' for day in days]
     # append the CSV files
     df_csv_concat = pd.concat([pd.read_csv(file) for file in csv_files ], ignore_index=True)
-    #df_csv_concat.iloc[:, 1:].to_csv("merged_data.csv")
-    print(df_csv_concat.shape[0] == sum(record))
+    df_csv_concat.iloc[:, 1:].to_csv("C:/Users/River/23summer/dku/edf_monkey/Data/Mercury/merged_data.csv")
  
 def find_csv_filenames( path_to_dir, suffix=".csv"):
     filenames = os.listdir(path_to_dir)
@@ -40,9 +35,15 @@ def merge_beh(days):
         df_csv_concat = pd.concat([pd.read_csv("C:/Users/River/23summer/dku/edf_monkey/Data/Mercury/" + day + "/M_" + day + '_BHV/'+file) for file in csv_files ], ignore_index=True)
         df_csv_concat.iloc[:, 1:].to_csv("C:/Users/River/23summer/dku/edf_monkey/Data/Mercury/" + day + "/M_" + day + '_BHV/2023' + day+'_bhv.csv')
 
+def merge_beh_all(days, monkey):
+    expect_dir = f'C:/Users/River/23summer/dku/edf_monkey/Data/{monkey}/'
+    csv_files = ['' + expect_dir + day + "/M_" + day + '_BHV/2023' + day+'_bhv.csv' for day in days]
+    # append the CSV files
+    df_csv_concat = pd.concat([pd.read_csv(file) for file in csv_files ], ignore_index=True)
+    df_csv_concat.iloc[:, 1:].to_csv("C:/Users/River/23summer/dku/edf_monkey/Data/Mercury/merged_behavior.csv")
+
 if __name__ == '__main__':
     days = ['0506', '0507', '0518', '0519', '0520', '0521', '0525', '0526', '0527']
-    for day in days:
-        merge_one_day_eye(day, 'Mercury')
+    merge_beh_all(days, 'Mercury')
     #merge_one_day_eye('0304')
     #merge_beh(['0521', '0525', '0526', '0527'])
